64f07928b54ef070944b8cebc560618a2337fd07
email <jordanmorel.ca@gmail.com>
2024-07-15 23:56:36 -0500

    Se agrega el proyecto

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.header__nav-toggle');
+    const navList = document.querySelector('.header__nav-list');
+
+    navToggle.addEventListener('click', () => {
+        navList.classList.toggle('header__nav-list--visible');
+    });
+});
